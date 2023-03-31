a1e9ce3f8f603fb8e9580f8c732ada3078943d9d
tinjet_11 <tinjet1016@gmail.com>
2023-03-31 10:24:04 +0800

    template for you to integrate your code

---
+++ template.php
@@ -0,0 +1,58 @@
+<?php
+//include 'session.php';
+?>
+
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="mainpage.css">
+    <link rel="stylesheet" href="dashboard.css">
+    <script src="sidebar.js"></script>
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="sidebar" id="sidebar">
+            <h2>Menu</h2>
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
+            <ul>
+
+                <li><a href="main.php"><i class="fa-solid fa-house"></i>Home</a></li>
+                <li><a href="reservation_dashboard.php"><i class="fa-sharp fa-solid fa-file"></i>Reservation_Dashboard</a></li>
+                <li><a href="reservation.php"><i class="fa-sharp fa-solid fa-file"></i>New Reservation</a></li>
+                <li><a href="customer_dashboard.php"><i class="fa-solid fa-car"></i>Customer_Dashboard</a></li>
+                <li><a href="#"><i class="fa-sharp fa-solid fa-eye"></i>Admin_Dashboard</a></li>
+                <li><a href="#"><i class="fa-sharp fa-solid fa-database"></i>Vehicle_Dashboard</a></li>
+            </ul>
+        </div><!-- end of sidebar -->
+
+        <div class="main_content" id="main_content">
+
+            <div class="header" id="header">
+                <button class="openbtn" id="openbtn" onclick="openNav()">☰ </button>
+                Premier Car Rental Agency
+                <div class="dropdown" style="float:right;">
+                    <button class="dropbtn"><i class="fa-solid fa-user"></i></button>
+                    <div class="dropdown-content">
+                        <a href="#"><i class="fa fa-home"></i> Profile </a>
+                        <a href="logout.php"><i class="fa-solid fa-right-from-bracket"></i> Logout </a>
+                    </div>
+                </div>
+
+            </div><!-- end of header-->
+
+            <!-- put your code here-->
+            <!-- put your code here-->
+            <!-- put your code here-->
+
+        </div><!-- end of main content-->
+    </div><!-- end of container-->
+
+
+
+</body>
+
+</html>
