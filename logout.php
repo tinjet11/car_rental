e1187b5ba5b25f5b32d16243b16d56f9b256d464
Lua Chong En <114934029+enn-y@users.noreply.github.com>
2023-04-04 23:39:33 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
-	session_destroy();
-	header('Location: login.php');
+	session_start(); // Initializes a new session/ resume a existing session
+	session_destroy(); // Destroys all data in session
+	header('Location: login.php'); // Redirects user to login page
 ?>
