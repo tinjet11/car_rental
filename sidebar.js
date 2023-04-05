850807bd9ef3224943c9da7d8d9e604a1cc2b842
Lua Chong En <114934029+enn-y@users.noreply.github.com>
2023-04-05 21:54:01 +0800

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -3,7 +3,7 @@
     document.getElementById("sidebar").style.width = "250px";
     document.getElementById("main_content").style.marginLeft = "250px";
     // document.getElementById("header").style.width= "87%";
-    document.getElementById("openbtn").style.display = "none";
+    document.getElementById("openbtn").style.display = "none"; /* Hides the open button and shows the close button */
   }
 
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
@@ -11,11 +11,11 @@
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("main_content").style.marginLeft = "0";
     //document.getElementById("header").style.width= "100%";
-    document.getElementById("openbtn").style.display = "block";
+    document.getElementById("openbtn").style.display = "block"; /* Shows the open button and hides the close button */
 
   }
 
-  window.addEventListener('resize', () => {
+  window.addEventListener('resize', () => { /* If window width is greater than 768 pixels then the sidebar width and main content will be set to 250, else set to 0. To ensure that sidebar is hidden when window width is small */
     if (window.innerWidth > 768) {
       document.getElementById("sidebar").style.width = "250px";
       document.getElementById("main_content").style.marginLeft = "250px";
